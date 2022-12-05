7176285bf4dddc2287e489acd7818099eea6fc3e
minninn <rn2685rn@gmail.com>
2022-12-05 15:39:01 +0900

    dvwa_attack

--- attack.sh
+++ attack.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-CSRF=$(cat /var/www/html/dvwa/includes/dvwaPage.inc.php | grep md5 | cut -d " " -f6)
+CSRF_PATH='/var/www/html/dvwa/includes/dvwaPage.inc.php'
+
+CSRF=$(cat $CSRF_PATH | grep md5 | cut -d " " -f6)
 
 if [ $CSRF = "uniqid()" ];then
-    sed -i 's/uniqid()/"1234"/g' /var/www/html/dvwa/includes/dvwaPage.inc.php
+    sed -i 's/uniqid()/"1234"/g' $CSRF_PATH
     echo 'CHANGE CSRF_TOKEN'
 fi
 
 python3 attack.py
 
+sed -i 's/"1234"/uniqid()/g' $CSRF_PATH
+echo 'RESET CSRF_TOKEN'
